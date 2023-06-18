f35c4848e43f35824778db41e2cf3968ea7af769
“lefts1der” <vildan321@mail.ru>
2023-06-18 12:40:39 -0700

    Cherry pick and refactor api

--- Sensors/views.py
+++ Sensors/views.py
@@ -9,47 +9,82 @@ from django.core.paginator import Paginator
 import json
 # Create your views here.
 
+
 def index(request):
     # Get all records and slice 15
     records = Sensor.objects.all()
     sensornames = list(set(records.values_list('sensorname', flat=True)))
-    print(sensornames)
     datatypes = list(set(records.values_list('datatype', flat=True)))
+    print(records[:15])
     return render(request,
                   'Sensors/index.html',
-                  {'records': records[:15],
-                   'sensornames': sensornames,
+                  {'sensornames': sensornames,
                    'datatypes': datatypes})
-def detailed(request, sensorname, position, datatype):
-    sensor_by_time = Sensor.objects.filter(sensorname=sensorname,
-                                           position=position,
-                                           datatype=datatype)
-    return JsonResponse(sensor_by_time)
 
-class APIView(View):
-    def get(self, request):
-        try:
-            print(request.body)
-            filter_params = json.loads(request.body)
-            filtered_sensors = Sensor.objects.filter(sensorname__in=filter_params['sensornames'],
-                                                     datatype__in=filter_params['datatypes']).order_by(filter_params["order_by"])
-            paginator = Paginator(filtered_sensors, 10)
-            num_pages = paginator.num_pages
+def graphics(request):
+    records = Sensor.objects.all()
+    sensornames = list(set(records.values_list('sensorname', flat=True)))
+    datatypes = list(set(records.values_list('datatype', flat=True)))
+    position = list(set(records.values_list('position', flat=True)))
+    return render(request,
+                  'Sensors/graphics.html',
+                  {'sensornames': sensornames,
+                   'datatypes': datatypes,
+                   'positions': position})
 
-            cur_page = (filter_params['page']-1)%num_pages + 1
-            
 
-            page_obj = paginator.get_page(cur_page)
-            response = {
-                'status': "ok",
-                'cur_page': cur_page,
-                'page_count': num_pages,
-                'data': [SensorAPISerializer(obj) for obj in page_obj]
-            }          
-        except:
-            return JsonResponse({"status":"error"})
-        else:
-            return JsonResponse(response)
+def filter_table(request):
+    try:
+        filter_params = json.loads(request.body)
+        filtered_sensors = Sensor.objects.filter(sensorname__in=filter_params['sensornames'],
+                                                 datatype__in=filter_params['datatypes']).order_by(filter_params["order_by"])
+        paginator = Paginator(filtered_sensors, 15)
+        num_pages = paginator.num_pages
+        cur_page = (filter_params['page']-1) % num_pages + 1
+
+        page_obj = paginator.get_page(cur_page)
+        response = {
+            'status': "ok",
+            'cur_page': cur_page,
+            'page_count': num_pages,
+            'data': [SensorAPISerializer(obj) for obj in page_obj]
+        }
+    except:
+        return JsonResponse({"status": "error"})
+    else:
+        return JsonResponse(response)
+
+def sensor_records(request):
+    try:
+        sensor_unique_fields = json.loads(request.body)
+        sensor_recors = Sensor.objects.filter(sensorname=sensor_unique_fields['sensorname'],
+                                              datatype=sensor_unique_fields['datatype'],
+                                              position=sensor_unique_fields['position'])
         
+        response = {
+            "status": "ok",
+            "records": [SensorAPISerializer(record) for record in sensor_recors]
+        }
+    except:
+        return JsonResponse({"status": "error"})
+    else:
+        return JsonResponse(response)
+
+def filter_update(request):
+    try:
+        selected_filters = json.loads(request.body)
+
+        records = Sensor.objects.filter(**selected_filters)
+        sensornames = list(set(records.values_list('sensorname', flat=True)))
+        datatypes = list(set(records.values_list('datatype', flat=True)))
+        positions = list(set(records.values_list('position', flat=True)))
         
-    
+        response = {
+            "sensornames": sensornames,
+            "datatypes": datatypes,
+            "positions": positions
+        }
+    except:
+        return JsonResponse({"status": "error"})
+    else:
+        return JsonResponse(response)

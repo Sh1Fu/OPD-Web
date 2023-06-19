834de3d7357a96b585ef3e922c0f313fb4a033a8
“lefts1der” <vildan321@mail.ru>
2023-06-19 16:27:36 -0700

    sda

--- Sensors/urls.py
+++ Sensors/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('graphics', views.graphics, name='graphics'),
-    path('api/', include([
+    path('apis/', include([
         path('update_filters', views.filter_update, name='update_filters_api'),
         path('table_filter', views.filter_table, name='table_filter_api'),
         path('sensor_records', views.sensor_records, name='sensor_records_api'),

e8f93023b09bbddc5ec68793da575b7ca4680f86
“lefts1der” <vildan321@mail.ru>
2023-06-22 16:42:40 -0700

    Add default secret key

--- SensorNet/settings.py
+++ SensorNet/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY', default='super_s3cr3t_key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
